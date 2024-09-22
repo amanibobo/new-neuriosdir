e4144daca700c922979d8d103ec17b03212e2c6c
Amani Bobo <amanibobo1@gmail.com>
2024-09-22 14:04:56 -0400

    esc

--- components/magicui/grid-flicker.tsx
+++ components/magicui/grid-flicker.tsx
@@ -43,8 +43,8 @@ const FlickeringGrid: React.FC<FlickeringGridProps> = ({
       if (!ctx) return "rgba(255, 0, 0,";
       ctx.fillStyle = color;
       ctx.fillRect(0, 0, 1, 1);
-      const [r, g, b] = ctx.getImageData(0, 0, 1, 1).data;
-      return `rgba(${r}, ${g}, ${b},`;
+      const data = ctx.getImageData(0, 0, 1, 1).data;
+      return `rgba(${data[0]}, ${data[1]}, ${data[2]},`;
     };
     return toRGBA(color);
   }, [color]);
